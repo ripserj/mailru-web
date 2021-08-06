3f8f51d95db244fc9e254cec9a73cdf1f6da2d55
ripserj <all4tv@yandex.ru>
2021-08-06 16:57:23 +0300

    structure

--- init.sh
+++ init.sh
@@ -1,21 +1,21 @@
-sudo mkdir public
-sudo chmod 777 public
-sudo mkdir uploads
-sudo chmod 777 uploads
-sudo mkdir etc
-sudo chmod 777 etc
-cd public
-sudo touch file1
-cd ..
-cd uploads
-sudo touch file2
-cd ..
-cd etc
-sudo touch file3
+#sudo mkdir public
+#sudo chmod 777 public
+#sudo mkdir uploads
+#sudo chmod 777 uploads
+#sudo mkdir etc
+#sudo chmod 777 etc
+#cd public
+#sudo mkdir css
+#sudo mkdir img
+#sudo mkdir js
+#sudo touch file1
+#cd ..
+#cd uploads
+#sudo touch file2
+#cd ..
+#cd etc
+#sudo touch file3
 
-
-#sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 ﻿﻿

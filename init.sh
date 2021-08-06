82be22ae66638a96423a376664d116c9da392b12
ripserj <all4tv@yandex.ru>
2021-08-06 15:09:36 +0300

    Creating dir-structure

---
+++ init.sh
@@ -0,0 +1,21 @@
+sudo mkdir public
+sudo chmod 777 public
+sudo mkdir uploads
+sudo chmod 777 uploads
+sudo mkdir etc
+sudo chmod 777 etc
+cd public
+sudo touch file1
+cd ..
+cd uploads
+sudo touch file2
+cd ..
+cd etc
+sudo touch file3
+
+
+#sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+﻿﻿
